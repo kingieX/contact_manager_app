81963142374a4ba7cbe0cb0e20238d2859f47910
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-09-26 17:19:02 +0100

    successfully completed authentication for user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
 // Schema for user registration
-// const userSchema = mongoose.Schema({
-//     username: {
-//         type: String,
-//         required: [true, "Please add the username"],
-//     },
-//     email: {
-//         type: String,
-//         required: [true, "Please add the email address"],
-//         unique: [true, "Email address already exist!"],
-//     },
-//     password: {
-//         type: String,
-//         required: [true, "Please add a password"],
-//     }
-// }, {
-//     timestamps: true
-// }
-// );
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the email address"],
+        unique: [true, "Email address already exist!"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+    }
+}, {
+    timestamps: true
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,24 +11,3 @@ router.post("/login", loginUser);
 router.get("/current", currentUser);
 
 module.exports = router;
-
-ggggggg
-
-const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please add the username"],
-    },
-    email: {
-        type: String,
-        required: [true, "Please add the email address"],
-        unique: [true, "Email address already exist!"],
-    },
-    password: {
-        type: String,
-        required: [true, "Please add a password"],
-    }
-}, {
-    timestamps: true
-}
-);
