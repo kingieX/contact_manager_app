c3cb9d9b85abf52179d13e59e35efcb15c63ef78
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-09-26 17:18:07 +0100

    successfully completed authentication for user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,3 +55,22 @@ const currentUser = asyncHandler(async (req, res) => {
 module.exports = { registerUser, loginUser, currentUser };
 
 ggggggggggggggg
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the email address"],
+        unique: [true, "Email address already exist!"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+    }
+}, {
+    timestamps: true
+}
+);
