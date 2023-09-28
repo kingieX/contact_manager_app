c64f2b86386db9f4e05cb2afbbe9197dfbe73588
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-09-28 15:16:06 +0100

    successfully allow multiple user creation and each user can perform CRUD on their individual contacts

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -5,7 +5,9 @@ const Contact = require("../models/contactModel");
 // @route GET /api/contacts
 // @access private
 const getContacts = asyncHandler(async (req, res) => {
+    // find individual contact by ID
     const contacts = await Contact.find({ user_id: req.user.id });
+    // return contact
     res.json(contacts).status(200);
 });
 
@@ -14,16 +16,19 @@ const getContacts = asyncHandler(async (req, res) => {
 // @access private
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body);
+    // inputs that is requested
     const {name, email, phoneNo} = req.body;
     if(!name || !email || !phoneNo) {
         throw new Error("Please input all the details")
     }
+    // create contact
     const contact = await Contact.create({
         name,
         email,
         phoneNo,
         user_id: req.user.id,
     });
+    // return contact created
     res.json(contact).status(201);
 });
 
@@ -31,11 +36,13 @@ const createContact = asyncHandler(async (req, res) => {
 // @route GET /api/contacts/:id
 // @access private
 const getContact = asyncHandler(async (req, res) => {
+    // check if contact is found
     const contact = await Contact.findById(req.params.id);
     if(!contact) {
         res.status(404);
         throw new Error("Contact not found!");
     }
+    // return contact
     res.json(contact).status(200);
 });
 
@@ -43,11 +50,19 @@ const getContact = asyncHandler(async (req, res) => {
 // @route PUT /api/contacts/:id
 // @access private
 const updateContact = asyncHandler(async (req, res) => {
+    // check if contact is found
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
         throw new Error("Contact not found!");
     }
 
+    // check if request match with user ID
+    if(contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to update other user contacts!!!");
+    }
+
+    // update contact
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -61,13 +76,20 @@ const updateContact = asyncHandler(async (req, res) => {
 // @route DELETE /api/contacts/:id
 // @access private
 const deleteContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
+    // check if contact is found
+    const contact = await Contact.findByIdAndRemove(req.params.id);
     if (!contact) {
         throw new Error("Contact not found!");
     }
+    // check if request match with user ID
+    if(contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to update other user contacts!!!");
+    }
 
-    const deletedContact = await Contact.deleteOne();
-    res.json(deletedContact).status(200);
+    // delete contact
+    const deletedContact = await contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(deletedContact);
 });
 
 
